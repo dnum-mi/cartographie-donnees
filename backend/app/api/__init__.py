ef71536e461fb7e1d45fdc3e6f7faed7f1d9bddf
Yann Carfantan <yann.carfantan@artelys.com>
2022-07-13 14:04:01 +0200

    Fix API error handling

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 api = Blueprint('api', __name__)
 
-from . import applications, auth, data_sources, enumerations, users
+from . import applications, auth, data_sources, enumerations, users, errors
