bd2aa0a4eded9c33df60cb6a0294dd4c650e2eee
Erick Ahmouck <ahmouckerick@gmail.com>
2022-09-12 16:39:02 +0200

    Change WildCard primary key to composite

--- backend/app/models/WildCard.py
+++ backend/app/models/WildCard.py
@@ -2,7 +2,7 @@ from app import db
 from app.models import BaseModel
 
 class WildCard(BaseModel):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    key = db.Column(db.String, nullable=False)
+    namespace = db.Column(db.String, nullable=False, primary_key = True)
+    key = db.Column(db.String, nullable=False, primary_key = True)
     value = db.Column(db.String)
-    namespace = db.Column(db.String)
+
