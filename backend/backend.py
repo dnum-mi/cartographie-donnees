b0e7767e734b99aff4f3a0a9e49781f5c5d730f4
Yann Carfantan <yann.carfantan@artelys.com>
2022-10-04 17:19:01 +0200

    Add logs for elasticsearch indexation

--- backend/backend.py
+++ backend/backend.py
@@ -5,5 +5,7 @@ from app.models import DataSource
 app = create_app()
 
 with app.app_context():
+    print("Indexing elasticsearch...")
     Application.reindex()
     DataSource.reindex()
+    print("Indexing ended...")
