3e56bd00294b9d581c8274fb25751d05b8b235f1
titigmr <56739959+titigmr@users.noreply.github.com>
2023-03-30 14:28:27 +0000

    Fix: email sending

--- backend/app/emails.py
+++ backend/app/emails.py
@@ -16,4 +16,4 @@ def send_email(subject, recipients, text_body, html_body):
     msg = Message(subject, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    Thread(target=send_async_email, args=(current_app, msg)).start()
+    Thread(target=send_async_email, args=(current_app._get_current_object(), msg)).start()
